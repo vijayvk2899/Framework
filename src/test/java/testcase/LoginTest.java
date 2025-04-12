d2a6a43c62c061afe63fc0457884ff6ebe344458
vijavk2899 <thevp231@gmail.com>
2025-04-12 13:14:37 +0530

    new

--- src/test/java/testcase/LoginTest.java
+++ src/test/java/testcase/LoginTest.java
@@ -7,10 +7,10 @@ import page.Login;
 public class LoginTest extends BaseClass {
 
     @Test
-    public void Loggin(){
-        Login log=new Login(driver);
-        log.signin(prop.getProperty("name"), prop.getProperty("pwd") );
+    public void Loggin() {
+        Login log = new Login(driver);
+        log.signin(prop.getProperty("name"), prop.getProperty("pwd"));
         log.click();
         System.out.println("Now trying gitbash");
-
+    }
 }
